591762a0258fc1eaf931494ba8be5d300b8cd4a7
Dave Lilleystone <davelilleystone@gmail.com>
2023-09-12 09:13:44 +0100

    remove js folder

--- js/script.js
+++
@@ -1,4 +0,0 @@
-const hb = document.querySelector(".navigation__hamburger");
-const mobileNav = document.querySelector(".navigation__links-wrapper");
-
-hb.addEventListener("click", () => mobileNav.classList.toggle("navigation__links-wrapper--open"));
